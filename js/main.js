ef6d7c59868f791d511596db860c62dc8fa22c8c
gizzSaw <gizzsaw@gmail.com>
2023-07-12 10:28:42 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const prevBtn = document.querySelector('.control__btn_prev')
+const nextBtn = document.querySelector('.control__btn_next')
+
+
+prevBtn.addEventListener('click', () => {
+    console.log('prevBtn clicked')
+})
+
+nextBtn.addEventListener('click', () => {
+    console.log('prevBtn clicked')
+})
